bc7bb25d32d03c4d877b61c3596d0bddec3e869a
TajievOlimjon <tajiev0711@gmail.com>
2023-10-26 09:36:45 +0500

    added relation course and group

--- WebApi/Entities/Course.cs
+++ WebApi/Entities/Course.cs
@@ -3,5 +3,8 @@
     public class Course : BaseEntity
     {
         public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public virtual ICollection<Group> Groups { get; set; } = null!;
     }
 }
+
--- WebApi/Entities/Group.cs
+++ WebApi/Entities/Group.cs
@@ -5,8 +5,11 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public DateTimeOffset? StartDate { get; set; } = null;
-        public virtual ICollection<Student> Students { get; set; } = null!;
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; } = null!;
+        /*public virtual ICollection<Student> Students { get; set; } = null!;*/
     }
 }
+
 
 
